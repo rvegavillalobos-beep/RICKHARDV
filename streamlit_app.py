b1e3ab26faa0a73d2b69ee2238a85b5c33c81cfe
rvegavillalobos-beep <r.vega.villalobos@gmail.com>
2026-09-02 16:22:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,33 @@
 # Ordenamos por fecha para simular el comportamiento secuencial del arreglo de VBA
-    df_raw = df_raw.sort_values(by="ParsedDate").reset_index(drop=True)
+        df_raw = df_raw.sort_values(by="ParsedDate").reset_index(drop=True)
 
-    base_keys = []
-    current_runs = []
-    run_tracker = {}
-    mod_corner_history = {}
+        base_keys = []
+        current_runs = []
+        run_tracker = {}
+        mod_corner_history = {}
 
-    for _, r_item in df_raw.iterrows():
-      f_date_str = str(r_item["ParsedDate"].date())
-      p_val = r_item[part_col]
-      base_key = f"{f_date_str}|{p_val}"
-      f_name = r_item[feat_col]
+        for _, r_item in df_raw.iterrows():
+          f_date_str = str(r_item["ParsedDate"].date())
+          p_val = r_item[part_col]
+          base_key = f"{f_date_str}|{p_val}"
+          f_name = r_item[feat_col]
 
-      if base_key not in run_tracker:
-        run_tracker[base_key] = 1
-        mod_corner_history[base_key] = f_name
-      else:
-        if f_name in mod_corner_history[base_key]:
-          run_tracker[base_key] += 1
-          mod_corner_history[base_key] = f_name
-        else:
-          mod_corner_history[base_key] += f";{f_name}"
+          if base_key not in run_tracker:
+            run_tracker[base_key] = 1
+            mod_corner_history[base_key] = f_name
+          else:
+            if f_name in mod_corner_history[base_key]:
+              run_tracker[base_key] += 1
+              mod_corner_history[base_key] = f_name
+            else:
+              mod_corner_history[base_key] += f";{f_name}"
 
-      base_keys.append(base_key)
-      current_runs.append(run_tracker[base_key])
+          base_keys.append(base_key)
+          current_runs.append(run_tracker[base_key])
 
-    # Asignamos las columnas de vuelta al DataFrame para evitar el KeyError
-    df_raw["BaseKey"] = base_keys
-    df_raw["CurrentRun"] = current_runs
+        # Asignamos las columnas de vuelta al DataFrame
+        df_raw["BaseKey"] = base_keys
+        df_raw["CurrentRun"] = current_runs
 
-    # Agrupamos por baseKey y Run para consolidar las 4 esquinas por batería/corrida
-    grouped_runs = df_raw.groupby(["BaseKey", "CurrentRun"])
+        # Agrupamos por baseKey y Run para consolidar las 4 esquinas por batería/corrida
+        grouped_runs = df_raw.groupby(["BaseKey", "CurrentRun"])
