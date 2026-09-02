f8eaad300823049d0f30773203892c9520d52dc1
rvegavillalobos-beep <r.vega.villalobos@gmail.com>
2026-09-02 13:49:38 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Battery Inspection App", layout="wide")
+st.title("🔋 Battery Dimensional Analytics Engine")
+st.write("El entorno se ha desplegado correctamente. Listos para cargar el código final.")
